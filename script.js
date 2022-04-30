0a2f7577de7607e1d7a5f29ce7d2488a2a485fbe
MAHAM <abdulkabore@gmail.com>
2022-04-30 11:48:12 +0000

    compute task amount add task remove tasks

--- script.js
+++ script.js
@@ -1,46 +1,44 @@
 // function to add task
-var add_task = function() {
+var add_task = function(){
 	// select parent div
-	let tasks_div = document.querySelector("body #parent .tasks")
+	let tasks_div = document.querySelector(".tasks")
 	let tasks_amount = document.querySelector("#tasks_amount")
-
-	//create elements for add tasks
-	let div = document.createElement("div")
-	let text_p = document.createElement("p")
-	let icone_p = document.createElement("p")
-	let button = document.createElement("button")
-	let i = document.createElement("i")
+	let input  = document.querySelector("#input")
 
 	// get values from field and index
 	let amount_text = tasks_amount.textContent
 	let index = Number(amount_text)+1
 
-	let value = document.querySelector("#input").value
+	let value = input.value
 
-	// set attributes (class and id to created div)
-	div.setAttribute("class", "under_tasks")
-	text_p.setAttribute("class", "text")
-	button.setAttribute("class", "icones")
-	i.setAttribute("class", "fa-solid fa-trash")
-	
-	let text = document.createTextNode(value)
-	// let amount = document.createTextNode(index)
+	let tasks = 
+	`
+		<div class="under_tasks">
+			<input type="checkbox" class="checkbox"/>
+			<p class="text">
+				${value}
+			</p>
+			<button id="icon_button" class="icones" onclick="remove_task(event)">
+					<i class="fa-solid fa-trash"></i>
+			</button>
+		</div>
+	`
 
 	// append all to html page
+	tasks_div.innerHTML += tasks
+
+	//set input to none
+	input.value = " "
 	tasks_amount.innerHTML = tasks_amount.innerHTML.replace(amount_text, index)
-	text_p.appendChild(text)
-	icone_p.appendChild(i)
-	button.appendChild(icone_p)
-	div.appendChild(text_p)
-	div.appendChild(button)
-	tasks_div.appendChild(div)
+
+	
 
 }
 
 // function to remove all tasks
 
 var remove_tasks = function (){
-	let tasks_div = document.querySelector("body #parent .tasks")
+	let tasks_div = document.querySelector(".tasks")
 	let last = tasks_div.lastElementChild
 
 	while (last)
@@ -55,26 +53,22 @@ var remove_tasks = function (){
 
 }
 
-var remove_task = function (){
-	let icones  = document.querySelectorAll("#icon_button")
-	// console.log(tasks_div)
-	// console.log(icone)
-	for (let i = 0; i < icones.length; i++)
-	{
-		this.parentElement.remove()
-	}
-	
+function remove_task(e){
+	let tasks_div = document.querySelector(".tasks")
+	let task_amount = document.querySelector("#tasks_amount")
+	let amount_text = task_amount.textContent
+	let index = Number(amount_text)-1
 
 
-	 
+	console.log(e.target)
+	tasks_div.removeChild(e.target.parentElement.parentElement)
+	tasks_amount.innerHTML = tasks_amount.innerHTML.replace(amount_text, index)
 }
 
 // select button add ans remove_all
 let add_button = document.querySelector("#add_button")
 let remove_All_button = document.querySelector("#remove_all")
-let delete_button = document.querySelector("#icon_button")
 
 // events listener
 add_button.addEventListener("click", add_task)
-delete_button.addEventListener("click", remove_task)
 remove_All_button.addEventListener("click", remove_tasks)
