3d7cb22c59bba68ee5f3c9bf3e9b7efecde89a26
MAHAM <abdulkabore@gmail.com>
2022-06-29 11:36:36 +0200

    update script.js

--- script.js
+++ script.js
@@ -1,80 +1,59 @@
-// function to add task
-var add_task = function() {
-	// select parent div
-	let tasks_div = document.querySelector("body #parent .tasks")
-	let tasks_amount = document.querySelector("#tasks_amount")
+let tasks_div = document.querySelector(".tasks")
+let tasks_amount = document.querySelector("#tasks_amount")
+let task_input = document.querySelector("#input")
 
-	//create elements for add tasks
-	let div = document.createElement("div")
-	let text_p = document.createElement("p")
-	let icone_p = document.createElement("p")
-	let button = document.createElement("button")
-	let i = document.createElement("i")
-
+let add_task = function() {
 	// get values from field and index
-	let amount_text = tasks_amount.textContent
-	let index = Number(amount_text)+1
-
-	let value = document.querySelector("#input").value
-
-	// set attributes (class and id to created div)
-	div.setAttribute("class", "under_tasks")
-	text_p.setAttribute("class", "text")
-	button.setAttribute("class", "icones")
-	i.setAttribute("class", "fa-solid fa-trash")
-	
-	let text = document.createTextNode(value)
-	// let amount = document.createTextNode(index)
-
-	// append all to html page
-	tasks_amount.innerHTML = tasks_amount.innerHTML.replace(amount_text, index)
-	text_p.appendChild(text)
-	icone_p.appendChild(i)
-	button.appendChild(icone_p)
-	div.appendChild(text_p)
-	div.appendChild(button)
-	tasks_div.appendChild(div)
-
+	tasks_div.innerHTML += newTask(task_input.value)
+	tasks_amount.textContent = Number(tasks_amount.textContent) + 1
 }
 
 // function to remove all tasks
 
-var remove_tasks = function (){
-	let tasks_div = document.querySelector("body #parent .tasks")
-	let last = tasks_div.lastElementChild
-
-	while (last)
-	{
-		tasks_div.removeChild(last)
-		last = tasks_div.lastElementChild
-	}
-
-	let tasks_amount = document.querySelector("#tasks_amount")
-	let amount_text = tasks_amount.textContent
-	tasks_amount.innerHTML = tasks_amount.innerHTML.replace(amount_text, 0)
-
+let remove_tasks = function (){
+	tasks_div.textContent = ""
+	tasks_amount.textContent = 0
 }
 
-var remove_task = function (){
-	let icones  = document.querySelectorAll("#icon_button")
-	// console.log(tasks_div)
-	// console.log(icone)
-	for (let i = 0; i < icones.length; i++)
-	{
-		this.parentElement.remove()
+let remove_task = function (e){
+
+	let par = e.target.parentElement.parentElement
+	console.log(par, par.parentElement)
+	tasks_div.removeChild(par)
+	tasks_amount.textContent = Number(tasks_amount.textContent) - 1
+}
+
+let newTask = (task_text)=>{
+	return (
+	`
+	<div class="under_tasks">
+		<input type="checkbox" class="checkbox"/>
+		<p class="text">
+			${task_text}
+		</p>
+		<button id="icon_button" class="icones" onclick="remove_task(event)">
+				<i class="fa-solid fa-trash"></i>
+		</button>
+	</div>
+	`
+	)
+}
+
+let handle = function (){
+	if (task_input.value){
+		add_button.disabled = false
+	}else{
+		add_button.disabled = true
 	}
-	
-
-
-	 
 }
 
 // select button add ans remove_all
 let add_button = document.querySelector("#add_button")
 let remove_All_button = document.querySelector("#remove_all")
 let delete_button = document.querySelector("#icon_button")
+add_button.disabled = true
 
 // events listener
 add_button.addEventListener("click", add_task)
-delete_button.addEventListener("click", remove_task)
 remove_All_button.addEventListener("click", remove_tasks)
+task_input.addEventListener("input", handle)
