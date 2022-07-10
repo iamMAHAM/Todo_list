f82724a19fc26ecf7c6e55cef3c149ec4cb52482
MAHAM <abdulkabore@gmail.com>
2022-07-10 13:28:26 +0200

    update route.js

--- route.js
+++ route.js
@@ -1,10 +1,18 @@
 const express = require("express")
+const fs = require("fs")
 const update = require("./middlewares/update")
 const router = express.Router()
 
 
 router.get("/", (req, res)=>{
     res.render("index")
+})
+
+router.get("/data", (req, res)=>{
+    fs.readFile("./tasks/tasks.json", (err, data)=>{
+        if (err) throw err
+        res.send(JSON.stringify(data.toString()))
+    })
 })
 
 router.post("/", (req, res)=>{
