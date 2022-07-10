04707e3ef8f908bd19ab871ba383ccbe0dba4928
MAHAM <abdulkabore@gmail.com>
2022-07-10 12:23:02 +0200

    update tasks.json path

--- middlewares/update.js
+++ middlewares/update.js
@@ -1,7 +1,7 @@
 const fs = require("fs")
 
 const update = (data={})=>{
-    fs.writeFile("./assets/tasks.json", JSON.stringify(data, null, 4), (err)=>{
+    fs.writeFile("./tasks/tasks.json", JSON.stringify(data, null, 4), (err)=>{
         if (err) throw err
     })
 }
